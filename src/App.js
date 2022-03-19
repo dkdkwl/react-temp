5cc3388b96b55a17696fd4c6abc2a9c2b6b34860
tubi55 <tubi55@nate.com>
2022-03-19 14:38:39 +0900

    컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './scss/style.scss';
+import Header from './component/Header';
+import Footer from './component/Footer';
+
+/*
+  JSX : HTML문법과 비슷하게 동적으로 DOM을 생성해주는 문법 체계 (가상DOM :VirtualDOM)
+  Component : JSX를 통해서 특정 기능을 하는 가상DOM구조를 재활용 가능하게 함수형태로 패키징
+*/
 
 function App() {
   return (
     <div className="App">  
-      <h1>title1</h1>
-      <h2>title2</h2>
-      <h3>title3</h3>
-      <h4>title4</h4>
+      <Header />
+
+      <section>
+        <article>1</article>
+        <article>2</article>
+        <article>3</article>
+      </section>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,10 @@
+//rfce
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
