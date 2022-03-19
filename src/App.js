ab2b062adc3c8a98b4384ad863269b99cf7e33ef
tubi55 <tubi55@nate.com>
2022-03-19 14:03:03 +0900

    react template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <h1>title1</h1>
+      <h2>title2</h2>
+      <h3>title3</h3>
+      <h4>title4</h4>
     </div>
   );
 }
