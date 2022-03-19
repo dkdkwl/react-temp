61681a9adada8c2af97c03afb86f1ac3a1398b16
tubi55 <tubi55@nate.com>
2022-03-19 14:58:40 +0900

    컴포넌트 안쪽에 배열의 갯수만큼 가상돔 생성하면서 데이터 바인딩

--- src/component/Section.js
+++ src/component/Section.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+/*
+  JSX안에서는 삼항연산자, map, 변수치환만 가능
+  해당 연산이 필요한 구문은 {}로 감싸줌
+*/
+
 function Section() {
   const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
 
   return (
     <section>
-      <article>1</article>
-      <article>2</article>
-      <article>3</article>
+      {arr.map((data, idx)=>{
+        return (
+          <article key={idx}>{data}</article>
+        )
+      })}
     </section>
   )
 }
