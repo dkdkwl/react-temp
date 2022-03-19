6cbfe395125d70a8e5c4e979a269db10fa2162cd
tubi55 <tubi55@nate.com>
2022-03-19 14:46:45 +0900

    레이아웃 컴포넌트 분리 및 fragment

---
+++ src/component/Section.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Section() {
+  const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
+
+  return (
+    <section>
+      <article>1</article>
+      <article>2</article>
+      <article>3</article>
+    </section>
+  )
+}
+
+export default Section;
