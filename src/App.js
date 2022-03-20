7994fa02580006e7127a1512f923e8ac8d904882
tubi55 <tubi55@nate.com>
2022-03-20 15:03:34 +0900

    이벤트 발생시 대입형 함수 연결

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import './scss/style.scss';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Section from './component/Section';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  let [num, setNum] = useState(0);
+
+  const movePrev = () => {
+    setNum(--num);
+    console.log(num);
+  }
+
+  const moveNext = () => {
+    setNum(++num);
+    console.log(num);
+  }
+
   return (    
       <>
         <Header /> 
         <Section />
         <Footer />  
+
+        <div className="prev" onClick={movePrev}></div>
+        <div className="next" onClick={moveNext}></div>
       </>
   );
 }
 
-export default App;
