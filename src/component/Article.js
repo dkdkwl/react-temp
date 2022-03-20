7e5dcd3913ed030af62d8268280940415d5cf47f
tubi55 <tubi55@nate.com>
2022-03-20 14:24:56 +0900

    jsx에 스타일 적용

--- src/component/Article.js
+++ src/component/Article.js
@@ -1,9 +1,12 @@
 import React from 'react';
 const path = process.env.PUBLIC_URL;
 
-function Article(props) {   
+function Article(props) {  
+  //적용할 스타일을 객체로 선언
+  const style = {transform: 'rotate(45deg)', opacity: 0.5} 
   return (
-    <article>      
+    // 적용할 가상DOM에 인라인으로 연결
+    <article style={style}>      
       <div className='inner'>
         <h2>{props.data}</h2>
 
