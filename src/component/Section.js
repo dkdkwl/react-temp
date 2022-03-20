8836f733621cb872006064fe1554a461dd845f2b
tubi55 <tubi55@nate.com>
2022-03-20 14:10:13 +0900

    state변경시 자동으로 해당 컴포넌트 재 랜더링

--- src/component/Section.js
+++ src/component/Section.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Article from './Article';
+
+/*
+  리액트로 화면 개발이 효율적인 이유
+  -컴포넌트로 화면이 구성됨
+  -재사용 가능한 화면을 컴포넌트 단위로 재활용가능
+  -모든 컴포넌트들이 state라는 정보값에 따라서 자동으로 화면이 재 랜더링
+
+  리액트 컴포넌트 요소에 이벤트 연결
+  JSX구문 안에서 직접 이벤트 핸들러 함수 연결
+
+  useState는 배열을 반환 [정보값을 저장하는 state, 해당state를 변경할 수 있는 함수]
+  useState state를 생성하고 해당 state값은 무조건 setState로만 변경 가능
+*/
 
 function Section() {
   const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
+  const [data, setData] = useState(arr);
+  console.log(data);
+  console.log(setData);    
 
   return (
-    <section>
-      {arr.map((data, idx)=>{
-        return <Article key={idx} data={data} />
-      })}
-    </section>
+    <>
+      {/* 버튼 클릭시 */}
+      <button onClick={()=>{
+        //기존의 data state를 전개연산자로  deepCopy한 다음
+        let newData = [...data];
+        //복사된 새로운 배열값을 변경하고
+        newData[0] = 'Calm';
+        //변경된 배열값을 state전용변경함수로 바꿔치기
+        setData(newData);
+      }}>이미지 변경</button>
+
+      <section>
+        {/* state값이 변경이 일어나면 자동으로 해당 컴포넌트는 재 랜더링 발생 */}
+        {data.map((data, idx)=>{
+          return <Article key={idx} data={data} />
+        })}
+      </section>
+    </>
   )
 }
 
