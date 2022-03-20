972f763253e24468b1a647f151c07545033a5089
tubi55 <tubi55@nate.com>
2022-03-20 15:18:03 +0900

    좌우버튼 클릭시 프레임 회전, useRef로 가상DOM 참조

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import './scss/style.scss';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Section from './component/Section';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function App() {
   let [num, setNum] = useState(0);
+  let frame = useRef(null);  
 
   const movePrev = () => {
-    setNum(--num);
+    setNum(++num);
     console.log(num);
+    frame.current.style = `transform: rotate(${45*num}deg)`;    
   }
 
   const moveNext = () => {
-    setNum(++num);
+    setNum(--num);
     console.log(num);
+    frame.current.style = `transform: rotate(${45*num}deg)`; 
   }
 
   return (    
       <>
         <Header /> 
-        <Section />
+        <Section frame={frame} />
         <Footer />  
 
         <div className="prev" onClick={movePrev}></div>
--- src/component/Section.js
+++ src/component/Section.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import Article from './Article';
 
-function Section() {
+function Section(props) {
   const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
   const [data, setData] = useState(arr);  
   const deg = 360/arr.length;
 
   return (
     <>
-      <section className='wrap'>
+      <section className='wrap' ref={props.frame}>
         {data.map((data, idx)=>{
           return <Article key={idx} data={data} deg={deg} idx={idx} />
         })}
