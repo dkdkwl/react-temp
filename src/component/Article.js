4eb83d5bcb7d29b2a57f662f5b8bcd6a7802067c
tubi55 <tubi55@nate.com>
2022-03-20 13:30:05 +0900

    ssr, csr방식의 이해

--- src/component/Article.js
+++ src/component/Article.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+//public폴더의 절대 경로값
+const path = process.env.PUBLIC_URL;
 
 //비구조하 할당으로 해당 객체의 값을 바로 키값으로 전달가능
 function Article(props) { 
   console.log(props);  
   return (
-    <article>
+    <article>      
       <h2>{props.data}</h2>
-      <img src={props.imgSrc} />
+      {/* public폴더까지의 절대경로를 불러와서 그 뒤에 props로 전달받은 이미지명을 이용해 이미지 소스 url완성 */}
+      <img src={`${path}/img/${props.data}.jpg`} />
     </article>
   )
 }
--- src/component/Section.js
+++ src/component/Section.js
@@ -1,21 +1,5 @@
 import React from 'react';
 import Article from './Article';
-//image import 
-import pic1 from '../img/Blizzards.jpg'; 
-import pic2 from '../img/Calm.jpg'; 
-import pic3 from '../img/Dusty_Road.jpg'; 
-import pic4 from '../img/Escape.jpg'; 
-import pic5 from '../img/Payday.jpg'; 
-import pic6 from '../img/Retreat.jpg'; 
-import pic7 from '../img/Seasonal.jpg'; 
-import pic8 from '../img/Vespers.jpg'; 
-const imgs = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
-
-/*
-  react 단방향 데이터 전달방식 (부모->자식)
-  부모컴포넌트에서 자식 컴포넌트에 prop으로 원하는 데이터를 전달
-  자식컴포넌트에서는 props라는 객체로 해당 데이터를 전달 받는
-*/
 
 function Section() {
   const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
@@ -23,7 +7,7 @@ function Section() {
   return (
     <section>
       {arr.map((data, idx)=>{
-        return <Article key={idx} data={data} imgSrc={imgs[idx]} />
+        return <Article key={idx} data={data} />
       })}
     </section>
   )
