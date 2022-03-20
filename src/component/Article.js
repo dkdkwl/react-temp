6fbd290fcc99c13b6b73942a45f6aede8114f8d1
tubi55 <tubi55@nate.com>
2022-03-20 12:49:56 +0900

    이미지 불러오기

--- src/component/Article.js
+++ src/component/Article.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 //비구조하 할당으로 해당 객체의 값을 바로 키값으로 전달가능
-function Article(props) {  
+function Article(props) { 
+  console.log(props);  
   return (
-    <article>{props.data}</article>
+    <article>
+      <h2>{props.data}</h2>
+      <img src={props.imgSrc} />
+    </article>
   )
 }
 
-export default Article
+export default Article;
--- src/component/Section.js
+++ src/component/Section.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import Article from './Article';
+//image import 
+import pic1 from '../img/Blizzards.jpg'; 
+import pic2 from '../img/Calm.jpg'; 
+import pic3 from '../img/Dusty_Road.jpg'; 
+import pic4 from '../img/Escape.jpg'; 
+import pic5 from '../img/Payday.jpg'; 
+import pic6 from '../img/Retreat.jpg'; 
+import pic7 from '../img/Seasonal.jpg'; 
+import pic8 from '../img/Vespers.jpg'; 
+const imgs = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
 
 /*
   react 단방향 데이터 전달방식 (부모->자식)
@@ -7,14 +17,13 @@ import Article from './Article';
   자식컴포넌트에서는 props라는 객체로 해당 데이터를 전달 받는
 */
 
-
 function Section() {
   const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
 
   return (
     <section>
       {arr.map((data, idx)=>{
-        return <Article key={idx} data={data}/>
+        return <Article key={idx} data={data} imgSrc={imgs[idx]} />
       })}
     </section>
   )
